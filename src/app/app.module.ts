5f1135a37dc098ef2cbc83aec248963c84332254
Anderson Dourado <anderson.ictus@gmail.com>
2019-05-14 13:20:07 -0300

    0.2.2 - Novo Layout ------------------------------ - Mudanças no Modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppComponent } from './app.component';
-import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule,
          MatListModule, MatInputModule, MatFormFieldModule, MatCardModule,
          MatSelectModule, MatOptionModule, MatProgressSpinnerModule, MatTableModule,
          MatPaginatorModule, MatSortModule } from '@angular/material';
-
+import { MatDialogModule } from '@angular/material/dialog';
 import { LayoutModule } from '@angular/cdk/layout';
 
-import { AppRoutingModule } from './app-routing.module';
+import { GeneralModule } from './general/general.module';
+import { ContabilistaModule } from './main/contabilista/contabilista.module';
+
+import { AppComponent } from './app.component';
 
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
 
 import { MainComponent } from './main/main.component';
-import { ContabilistaModule } from './main/contabilista/contabilista.module';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-
-import { GeneralModule } from './general/general.module';
 import { TemasComponent } from './main/temas/temas.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-
-    LoginComponent,
-    MainComponent,
-    TemasComponent
-  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
@@ -43,25 +37,39 @@ import { TemasComponent } from './main/temas/temas.component';
     ContabilistaModule,
     GeneralModule,
 
+    // MATERIAL MODULES
     LayoutModule,
     MatDialogModule,
     MatToolbarModule,
-    MatButtonModule,
     MatSidenavModule,
+
     MatIconModule,
     MatListModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatCardModule,
-    MatSelectModule,
-    MatOptionModule,
-    MatProgressSpinnerModule,
 
+    MatCardModule,
+    MatOptionModule,
+
+    // FORMS AND COMPOENNETS MODULES
+    MatFormFieldModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    MatInputModule,
+    MatButtonModule,
+
+    // TABLE MODULE
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
 
-    NgbModule.forRoot()
+    NgbModule
+  ],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+
+    LoginComponent,
+    MainComponent,
+    TemasComponent
   ],
   providers: [ ],
   bootstrap: [AppComponent]
